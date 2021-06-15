7a3793dc2da7dae8c0619c3f03a61f1d2d5bf029
petrovyuri <mr.developer@yandex.ru>
2021-06-15 09:12:03 +0300

    isRoot

--- route_gen/lib/src/generators/routers_generator.dart
+++ route_gen/lib/src/generators/routers_generator.dart
@@ -11,11 +11,15 @@ class RoutersGenerator extends GeneratorForAnnotation<Routed> {
   generateForAnnotatedElement(
       Element? element, ConstantReader annotation, BuildStep buildStep) {
     if (element == null) return null;
+    var visitor = ModelVisitor();
+    var isRoot = visitor.mapFields["isRoot"];
+
     if (element.kind == ElementKind.CLASS && element.name != null) {
       var className = "${element.name!}";
       var ref = StringUtils.camelCaseToLowerUnderscore(className);
       if (listRefs.contains(ref)) return null;
-      listRefs.add("\$${className}Generator()");
+      listRefs.add(
+          "\$${className}Generator(isRoot: $isRoot)");
       return """
 import 'package:flutter/material.dart';
 import '../../../../app/presentation/router/app_route_generator.dart';
@@ -38,5 +42,137 @@ class \$${className}Generator implements RouteGenerator {
     }
 
     return null;
+  }
+}
+
+class ModelVisitor extends ElementVisitor {
+  Map<String, dynamic> mapFields = Map();
+
+  @override
+  visitClassElement(ClassElement element) {
+    // TODO: implement visitClassElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitCompilationUnitElement(CompilationUnitElement element) {
+    // TODO: implement visitCompilationUnitElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitConstructorElement(ConstructorElement element) {}
+
+  @override
+  visitExportElement(ExportElement element) {
+    // TODO: implement visitExportElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitExtensionElement(ExtensionElement element) {
+    // TODO: implement visitExtensionElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitFieldElement(FieldElement element) {
+    mapFields[element.name] = element.type;
+  }
+
+  @override
+  visitFieldFormalParameterElement(FieldFormalParameterElement element) {
+    // TODO: implement visitFieldFormalParameterElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitFunctionElement(FunctionElement element) {
+    // TODO: implement visitFunctionElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitFunctionTypeAliasElement(FunctionTypeAliasElement element) {
+    // TODO: implement visitFunctionTypeAliasElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitGenericFunctionTypeElement(GenericFunctionTypeElement element) {
+    // TODO: implement visitGenericFunctionTypeElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitImportElement(ImportElement element) {
+    // TODO: implement visitImportElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitLabelElement(LabelElement element) {
+    // TODO: implement visitLabelElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitLibraryElement(LibraryElement element) {
+    // TODO: implement visitLibraryElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitLocalVariableElement(LocalVariableElement element) {
+    // TODO: implement visitLocalVariableElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitMethodElement(MethodElement element) {
+    // TODO: implement visitMethodElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitMultiplyDefinedElement(MultiplyDefinedElement element) {
+    // TODO: implement visitMultiplyDefinedElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitParameterElement(ParameterElement element) {
+    // TODO: implement visitParameterElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitPrefixElement(PrefixElement element) {
+    // TODO: implement visitPrefixElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitPropertyAccessorElement(PropertyAccessorElement element) {
+    // TODO: implement visitPropertyAccessorElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitTopLevelVariableElement(TopLevelVariableElement element) {
+    // TODO: implement visitTopLevelVariableElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitTypeAliasElement(TypeAliasElement element) {
+    // TODO: implement visitTypeAliasElement
+    throw UnimplementedError();
+  }
+
+  @override
+  visitTypeParameterElement(TypeParameterElement element) {
+    // TODO: implement visitTypeParameterElement
+    throw UnimplementedError();
   }
 }
